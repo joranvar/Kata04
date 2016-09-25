15dfb9ebca95d876dc1ecc2c9021466ea74f7dc3
joranvar <bart.post@gmail.com>
2016-09-25 07:29:22 +0200

    Parse a record successfully

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,9 +3,17 @@ module Lib
   (
     -- * Domain types
     Weather (..)
+    -- * Functions
+  , parse
   ) where
 
 -- | A weather record
 data Weather = Weather { day::Int
                        , maxTemp::Float
                        , minTemp::Float }
+  deriving (Eq, Show)
+
+-- | Parsing a record from the file
+parse :: String  -- ^ The line from the file
+      -> Weather -- ^ The corresponding Weather data
+parse s = let (d:ma:mi:_) = words s in Weather (read d) (read ma) (read mi)
