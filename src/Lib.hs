458bf94d87b272825004cc982e65a63462b4e9f4
joranvar <bart.post@gmail.com>
2016-10-01 09:52:25 +0200

    Create type and write reader

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,20 @@
 -- | Kata04 - http://codekata.com/kata/kata04-data-munging/
 module Lib
   (
-    -- * Exported functions
+    -- * Domain types
+    Weather(..)
+
+    -- * Reading function
+  , readWeather
   ) where
+
+data Weather = Weather { day::Int
+                       , maxTemp::Int
+                       , minTemp::Int }
+  deriving (Eq, Show)
+
+readWeather :: String -- ^ The input line
+            -> Weather -- ^ The parsed data
+readWeather s =
+  let da:ma:mi:[] = map read . take 3 . words $ s
+  in Weather da ma mi
