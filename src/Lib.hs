65de9d1afcc0cd927e233afdbe35fbf685c78344
joranvar <bart.post@gmail.com>
2016-10-02 07:35:07 +0200

    Parse a string to a Weather record

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,13 +6,19 @@ module Lib
     -- * Domain data
     Weather(..)
   -- * Weather functions
+  , readWeather
   , spread
   ) where
 
 data Weather = Weather { day::Int
                        , minTemp::Int
                        , maxTemp::Int }
+  deriving (Eq, Show)
 
 -- | The temperature spread for the given day
 spread :: Weather -> Int
 spread Weather{..} = maxTemp - minTemp
+
+-- | Turn a string in a Weather record if the data is parseable
+readWeather :: String -> Maybe Weather
+readWeather _ = Just $ Weather 1 88 59
