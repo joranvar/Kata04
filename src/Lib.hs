f4a633a2a5cf9ad0aced0e783755b81b6e65c7f8
joranvar <bart.post@gmail.com>
2016-10-02 07:28:52 +0200

    Calculate spread of a day

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,18 @@
 -- | Kata04 - http://codekata.com/kata/kata04-data-munging/
+{-# LANGUAGE RecordWildCards #-}
+
 module Lib
   (
     -- * Domain data
     Weather(..)
+  -- * Weather functions
+  , spread
   ) where
 
 data Weather = Weather { day::Int
-                       , min::Int
-                       , max::Int }
+                       , minTemp::Int
+                       , maxTemp::Int }
+
+-- | The temperature spread for the given day
+spread :: Weather -> Int
+spread Weather{..} = maxTemp - minTemp
