24373394ffb54850d970fd9d75823c05ab55b26f
joranvar <bart.post@gmail.com>
2016-10-02 07:25:45 +0200

    Define Weather domain record

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,10 @@
 -- | Kata04 - http://codekata.com/kata/kata04-data-munging/
 module Lib
   (
-    -- * Exported functions
+    -- * Domain data
+    Weather(..)
   ) where
+
+data Weather = Weather { day::Int
+                       , min::Int
+                       , max::Int }
