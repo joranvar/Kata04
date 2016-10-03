b22988e1fddb055f621a61012ac05f3f9c19c074
joranvar <bart.post@gmail.com>
2016-10-03 07:01:07 +0200

    Add spread function

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,19 @@
 -- | Kata04 - http://codekata.com/kata/kata04-data-munging/
+{-# LANGUAGE RecordWildCards #-}
+
 module Lib
   (
     -- * Domain types
     Weather(..)
+    -- * Weather functions
+  , spread
   ) where
 
 -- | A parsed Weather record
 data Weather = Weather { day::Int
                        , maxTemp::Int
                        , minTemp::Int }
+
+-- | Get the temperature spread (max-min)
+spread :: Weather -> Int
+spread Weather{..} = maxTemp - minTemp
