5a2afaa4d4f621c32154ed568a271b062e18988d
joranvar <bart.post@gmail.com>
2016-10-03 07:00:58 +0200

    Add weather data

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,11 @@
 -- | Kata04 - http://codekata.com/kata/kata04-data-munging/
 module Lib
   (
-    -- * Exported functions
+    -- * Domain types
+    Weather(..)
   ) where
+
+-- | A parsed Weather record
+data Weather = Weather { day::Int
+                       , maxTemp::Int
+                       , minTemp::Int }
