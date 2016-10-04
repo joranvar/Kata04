5882881380689a794e853425e22b1384d7dde587
joranvar <bart.post@gmail.com>
2016-10-04 07:42:56 +0200

    Calculate the temperature spread of one record

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,18 @@
 -- | Kata04 - http://codekata.com/kata/kata04-data-munging/
+{-# LANGUAGE RecordWildCards #-}
+
 module Lib
   (
     -- * Domain data
     Weather(..)
+    -- * Weather functions
+  , spread
   ) where
 
 data Weather = Weather { day::Int
                        , maxTemp::Int
                        , minTemp::Int }
+
+-- | Get the temperature spread of a weather record
+spread :: Weather -> Int
+spread Weather{..} = maxTemp - minTemp
