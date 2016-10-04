96acc8b88f24a02f2abba5b6276f6a17c90ef9e1
joranvar <bart.post@gmail.com>
2016-10-04 07:46:34 +0200

    Test parse from file

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,12 +7,17 @@ module Lib
     Weather(..)
     -- * Weather functions
   , spread
+  , parse
   ) where
 
 data Weather = Weather { day::Int
                        , maxTemp::Int
                        , minTemp::Int }
+  deriving (Eq, Show)
 
 -- | Get the temperature spread of a weather record
 spread :: Weather -> Int
 spread Weather{..} = maxTemp - minTemp
+
+parse :: String -> Weather
+parse = undefined
