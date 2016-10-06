4abe78b62ae8d0a331c29f08ff0df45cfda7f911
joranvar <bart.post@gmail.com>
2016-10-06 13:02:06 +0200

    Extract mabyeRead

--- src/Lib.hs
+++ src/Lib.hs
@@ -25,10 +25,13 @@ data Weather = Weather { day::Int
                        , mn::Int }
   deriving (Eq, Show)
 
+maybeRead :: (Read a) => String -> Maybe a
+maybeRead = fmap fst . listToMaybe . reads
+
 -- | Parse a record from a string
 parse :: String -> Maybe Weather
 parse s =
-  case map (fmap fst . listToMaybe . reads) . take 3 $ words s of
+  case map maybeRead . take 3 $ words s of
     [Just dy, Just mxT, Just mnT] -> Just $ Weather dy mxT mnT
     _ -> Nothing
 
@@ -49,7 +52,7 @@ data Team = Team { name::String
 -- | Parse a record from a string
 parseT :: String -> Maybe Team
 parseT s =
-  case (take 1 . drop 1) *** (map (fmap fst . listToMaybe . reads)) $ splitAt 2 $ words s of
+  case (take 1 . drop 1) *** (map maybeRead) $ splitAt 2 $ words s of
     ([team], [_p, _w, _l, _d, Just f, _dash, Just a, _pts]) -> Just $ Team team f a
     _ -> Nothing
 
