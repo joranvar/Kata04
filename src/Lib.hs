821222999e3486852e01d903b7a4570425553e2b
joranvar <bart.post@gmail.com>
2016-10-07 10:24:17 +0200

    Describe spread function

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,16 @@
 -- | Kata04 - http://codekata.com/kata/kata04-data-munging/
+{-# LANGUAGE RecordWildCards #-}
+
 module Lib
   (
     -- * Domain types
     Weather(..)
+    -- * Weather functions
+  , spread
   ) where
 
 data Weather = Weather { day::Int, mnT::Int, mxT::Int }
   deriving (Eq, Show)
+
+spread :: Weather -> Int
+spread Weather{..} = mxT - mnT
