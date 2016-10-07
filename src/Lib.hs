9de2a7fbf83b56f631c8b6fab6b0dc045d0db00c
joranvar <bart.post@gmail.com>
2016-10-07 10:22:18 +0200

    Define Weather record type

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,9 @@
 -- | Kata04 - http://codekata.com/kata/kata04-data-munging/
 module Lib
   (
-    -- * Exported functions
+    -- * Domain types
+    Weather(..)
   ) where
+
+data Weather = Weather { day::Int, mnT::Int, mxT::Int }
+  deriving (Eq, Show)
