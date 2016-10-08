ec929eabc66c41e8e4c245d10992458512b72982
joranvar <bart.post@gmail.com>
2016-10-08 09:27:58 +0200

    Add spread function

--- src/Weather.hs
+++ src/Weather.hs
@@ -1,12 +1,17 @@
 -- | Kata04 - http://codekata.com/kata/kata04-data-munging/
+{-# LANGUAGE RecordWildCards #-}
+
 module Weather
   (
     -- * Domain types
     Weather(..)
+    -- * Functions
+  , spread
   ) where
 
 data Weather = Weather { dy::Int
                        , mxt::Int
+                       , mnt::Int
                        , avt::Int
                        , hdday::Int
                        , avdp::Double
@@ -20,3 +25,6 @@ data Weather = Weather { dy::Int
                        , mxr::Int
                        , mnr::Int
                        , avslp::Double }
+
+spread :: Weather -> Int
+spread Weather{..} = mxt - mnt
