377e9a4af6c9084700941e082e3f90459e90f982
joranvar <bart.post@gmail.com>
2016-10-09 09:57:23 +0200

    Test rest of Part Two

--- src-test/Main.hs
+++ src-test/Main.hs
@@ -8,7 +8,7 @@ import Test.Tasty.SmallCheck
 import Test.SmallCheck.Series
 
 import Weather (Weather(..), parse, parseFile, minDayBySpread)
-import Soccer (Soccer(..), parse) -- , parseFile, minTeamByDiffScores)
+import Soccer (Soccer(..), parse, parseFile, minTeamByDiffScores)
 instance (Monad m, Enum a, Bounded a) => Serial m a where
   series = generate (\d -> take d [minBound .. maxBound])
 
@@ -48,15 +48,13 @@ weatherTests dat =
 
 soccerTests :: String -> [TestTree]
 soccerTests dat =
-  let _header:record1:_rest = lines dat in
+  let header:record1:_rest = lines dat in
   [ testCase "Can parse first record" $
     Soccer.parse record1 @?= (Just $ Soccer "Arsenal" 79 36)
-  -- , testCase "Will skip header" $
-  --   parse header @?= Nothing
-  -- , testCase "Will skip empty line" $
-  --   parse emptyline @?= Nothing
-  -- , testCase "Can parse whole file" $
-  --   (length $ parseFile dat) @?= 30
-  -- , testCase "Can solve question of part one" $
-  --   (minDayBySpread $ parseFile dat) @?= 14
+  , testCase "Will skip header" $
+    Soccer.parse header @?= Nothing
+  , testCase "Can parse whole file" $
+    (length $ Soccer.parseFile dat) @?= 20
+  , testCase "Can solve question of part one" $
+    (minTeamByDiffScores $ Soccer.parseFile dat) @?= "Aston_Villa"
   ]
