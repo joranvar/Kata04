fe09f22eeabf288abbef3d8027c44490e88322c9
joranvar <bart.post@gmail.com>
2016-10-09 09:41:45 +0200

    Parse a record

--- src-test/Main.hs
+++ src-test/Main.hs
@@ -7,16 +7,19 @@ import Test.Tasty.HUnit
 import Test.Tasty.SmallCheck
 import Test.SmallCheck.Series
 
+import Weather (Weather(..), parse)
 instance (Monad m, Enum a, Bounded a) => Serial m a where
   series = generate (\d -> take d [minBound .. maxBound])
 
 main :: IO ()
-main = defaultMain $ testGroup "all-tests" tests
+main = do
+  weatherDat <- readFile "data/weather.dat"
+  defaultMain $ testGroup "all-tests" $ tests weatherDat
 
-tests :: [TestTree]
-tests =
+tests :: String -> [TestTree]
+tests weatherDat =
   [ testGroup "SmallCheck" scTests
-  , testGroup "Unit tests" huTests
+  , testGroup "Weather tests" $ weatherTests weatherDat
   ]
 
 scTests :: [TestTree]
@@ -25,8 +28,9 @@ scTests =
     \x -> id (x::Int) == x
   ]
 
-huTests :: [TestTree]
-huTests =
-  [ testCase "True is True" $
-    True @?= True
+weatherTests :: String -> [TestTree]
+weatherTests dat =
+  let _header:_emptyline:record1:_rest = lines dat in
+  [ testCase "Can parse first record" $
+    parse record1 @?= (Just $ Weather 1 88 59)
   ]
--- src/Weather.hs
+++ src/Weather.hs
@@ -3,6 +3,23 @@ module Weather
   (
     -- * Domain type
     Weather(..)
+    -- * Functionality
+  , parse
   ) where
 
+import Data.Maybe (listToMaybe)
+
 data Weather = Weather { dy::Int, mxT::Int, mnT::Int }
+  deriving (Eq, Show)
+
+maybeRead :: (Read a) => String -> Maybe a
+maybeRead = fmap fst . listToMaybe . reads
+
+maybeWord :: Int -> String -> Maybe String
+maybeWord n = listToMaybe . drop n . words
+
+parse :: String -> Maybe Weather
+parse s = Weather
+          <$> (maybeRead =<< maybeWord 0 s)
+          <*> (maybeRead =<< maybeWord 1 s)
+          <*> (maybeRead =<< maybeWord 2 s)
