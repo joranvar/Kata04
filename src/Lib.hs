34797253baccb3deea58dde71ca428509d7f5039
joranvar <bart.post@gmail.com>
2016-10-10 11:53:45 +0200

    Create Weather type

--- src/Lib.hs
+++
@@ -1,5 +0,0 @@
--- | Kata04 - http://codekata.com/kata/kata04-data-munging/
-module Lib
-  (
-    -- * Exported functions
-  ) where
---
+++ src/Weather.hs
@@ -0,0 +1,8 @@
+-- | Kata04 - http://codekata.com/kata/kata04-data-munging/
+module Weather
+  (
+    -- * Domain types
+    Weather(..)
+  ) where
+
+data Weather = Weather
